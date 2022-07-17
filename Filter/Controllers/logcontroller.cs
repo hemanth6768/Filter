269d2d7e4ddb7e4f9e2780f739991d84e603951c
Lenovo <vignesh.ssai@augmentolabs.com>
2022-07-17 13:05:27 +0530

    Add project files.

---
+++ Filter/Controllers/logcontroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class logcontroller : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("log/{id}/{name}")]
+        [Log]
+        public string get(int id , string name )
+        {
+            return " hello " + " " +  name  +  "  my id is"  + id;
+        }
+    }
+}
---
+++ Filter/Log.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace Filter
+{
+    public class Log : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            log("this is form onaction executred", context.RouteData);
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            log("this is form on action executed", context.RouteData);
+        }
+
+        private void log(string methodname , RouteData routedata)
+        {
+            var controllername = (string)routedata.Values["controller"];
+
+            var actionname = (string)routedata.Values["action"];
+
+            var id = (string)routedata.Values["id"];    
+            var name = (string)routedata.Values["name"];    
+
+            var message = string.Format("{0} controller :{1} action: {2} id: {3} name: {4} ", methodname, controllername, actionname,id,name);
+
+            Console.WriteLine(message, "hemanth filter");
+
+        }
+    }
+}
